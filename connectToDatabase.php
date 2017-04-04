02dcae20c4b6b210ba74b7cf070d56fa8b618bab
saurabhlevi <saurabhnegi9654313343@gmail.com>
2017-04-04 18:53:45 +0530

    login validation done

---
+++ connectToDatabase.php
@@ -0,0 +1,3 @@
+<?php
+    mysqli_connect('localhost', 'root', '', 'online_attendance_system') or die('Server Is Down');
+?>
--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <!DOCTYPE html>
 <?php
-    $invalid="";
-    if(isset($_POST['username']) && isset($_POST['password']) && !empty($_POST['username']) && !empty($_POST['password'])){
-        //check in database here
-        if(true){
-            header("Location: https://www.google.com");
-            die();
-        }else{
-            $invalid="Invalid Username Or Password !!!";
-        }
+    $resultFromValidation="";
+    if(isset($_GET['r'])){
+        $resultFromValidation="Invalid Username Or Password !!!";
     }
- ?>
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -22,7 +16,7 @@
         }
         form{
             position: absolute;
-            top: 110px;
+            top: 146px;
             left: 800px;
             border: 3px solid black;
             padding: 30px 30px;
@@ -41,6 +35,23 @@
             border-bottom: 1px solid whitesmoke;
             box-sizing: border-box;
         }
+        
+        input[type=text],input[type=password]:focus{
+            outline: none;
+        }
+        
+        input[type=submit]{
+            width: 30%;
+            padding: 10px 10px;
+            margin: 30px 20px;
+            border: 1px solid black;
+            border-radius: 5px;
+            background-color: gray;
+            font-size: 15px;
+            font-family: cursive;
+            color: black;
+            cursor: pointer;
+        }
         button{
             padding: 10px 10px;
             margin: 30px 35px;
@@ -56,21 +67,6 @@
             left: 985px;
             width: 200px;
         }
-        input[type=submit]{
-            width: 30%;
-            padding: 10px 10px;
-            margin: 30px 20px;
-            border: 1px solid black;
-            border-radius: 5px;
-            background-color: gray;
-            font-size: 15px;
-            font-family: cursive;
-            color: black;
-            cursor: pointer;
-        }
-        input[type=text],input[type=password]:focus{
-            outline: none;
-        }
         input[type=submit]:hover,button:hover{
             opacity: 0.5;
         }
@@ -78,10 +74,10 @@
     <body>
         <img src="res/frontPageLogo2.png" alt=" Attendance Matters " style="margin-left: 5%;margin-top: 10%"></img>
         <br/>
-        <div style="color: red;position: absolute;top: 120px;left: 910px;font: 15px;font-family: cursive"><?php echo $invalid;?></div>
-        <form action="index.php" method="POST">
-            <b>Username</b><input type="text" name="username" placeholder="Enter Username" required="true"></input>
-            <b>Password</b><input type="password" name="password" placeholder="Enter Password" required="true"></input>
+        <div style="color: red;position: absolute;top: 150px;left: 915px;"><?php echo $resultFromValidation;?></div>
+        <form action="validate.php" method="POST" autocomplete="off">
+            <input type="text" name="username" placeholder="Enter Username" required="true"></input>
+            <input type="password" name="password" placeholder="Enter Password" required="true"></input>
             <input type="submit" value="Log In"></input>
         </form>
         <a href="https://www.google.com"><button>I Am A Student !</button></a>     
